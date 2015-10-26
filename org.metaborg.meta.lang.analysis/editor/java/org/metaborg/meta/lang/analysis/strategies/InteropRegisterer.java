e0667f4b15214e32e69e32f2f9097632545ca175
Moritz Lichter <lichtemo@googlemail.com>
2015-10-26 18:12:46 +0100

    Adapt InteropRegisterer

--- org.metaborg.meta.lang.analysis/editor/java/org/metaborg/meta/lang/analysis/strategies/InteropRegisterer.java
+++ org.metaborg.meta.lang.analysis/editor/java/org/metaborg/meta/lang/analysis/strategies/InteropRegisterer.java
@@ -5,6 +5,6 @@ import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
 	public InteropRegisterer() {
-		super(new Strategy[] {});
+		super(new LibraryInitializer());
 	}
 }
