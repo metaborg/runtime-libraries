ae42a6e8277ff15291c34d1e89d39a96cd5228c7
Gabriel Konat <gabrielkonat@gmail.com>
2015-10-27 13:31:07 +0100

    Mark method as nullable

--- org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/primitives/ITaskEngineContext.java
+++ org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/primitives/ITaskEngineContext.java
@@ -1,7 +1,9 @@
 package org.metaborg.runtime.task.primitives;
 
+import javax.annotation.Nullable;
+
 import org.metaborg.runtime.task.engine.ITaskEngine;
 
 public interface ITaskEngineContext {
-    public abstract ITaskEngine taskEngine();
+    public abstract @Nullable ITaskEngine taskEngine();
 }
